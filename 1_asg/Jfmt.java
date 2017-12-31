f75b2af1319ea1ad5edd79ad91447f38f66152ee
Gabriel Larwood <glarwood@ucsc.edu>
2017-12-31 14:13:31 -0800

    Add stuff

--- 1_asg/Jfmt.java
+++ 1_asg/Jfmt.java
@@ -1,5 +1,7 @@
 class Jfmt {
    public static void main (String[] args){
-      System.out.println("Hello Annie!");       
+      System.out.println("Hello Annie! Colors don't matter");       
+
+      int i = 5; 
    }
 }
